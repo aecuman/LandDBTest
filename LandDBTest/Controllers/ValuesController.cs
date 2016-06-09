afc3f684e8aa5903e742f7319bef6e4e70dac03f
Anthony Ecuman <aecuman24@gmail.com>
2016-06-09 10:22:47 +0300

    Authorize index

--- LandDBTest/Controllers/ValuesController.cs
+++ LandDBTest/Controllers/ValuesController.cs
@@ -17,12 +17,12 @@ namespace LandDBTest.Controllers
 
 
         // GET: Values
-        [AllowAnonymous]
+        [Authorize]
       public ActionResult Index()
         {
            return View();
         }
-        [AllowAnonymous]
+        [Authorize]
         public ActionResult List(int? block, string vill_search, string county, string sortVal, string Filter_Value, int? Page_No)
         {
          
