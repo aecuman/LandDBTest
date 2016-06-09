38e425a05a55d2dd3f1428fcc42a37ab88efe9c0
Anthony Ecuman <aecuman24@gmail.com>
2016-06-09 11:49:46 +0300

    reference account

--- LandDBTest/Models/AccountViewModels.cs
+++ LandDBTest/Models/AccountViewModels.cs
@@ -50,9 +50,8 @@ namespace LandDBTest.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
