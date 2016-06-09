219a363c779ab12f945645ff4a1c83870e4cca40
Anthony Ecuman <aecuman24@gmail.com>
2016-06-09 07:42:15 +0300

    login warrups

--- LandDBTest/App_Start/RouteConfig.cs
+++ LandDBTest/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace LandDBTest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Values", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
